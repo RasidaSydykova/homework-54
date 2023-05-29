580a0a02a52c84eef593dc7df898d7cc0dfe5495
RasidaSydykova <sydykova.rasida@mail.ru>
2023-05-29 16:26:04 +0600

    homework-54

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import PlayingField from "./components/PlayingField";
 import './App.css';
+import TriesCounter from "./components/TriesCounter";
+import ResetButton from "./components/ResetButton";
 
-function App() {
+const App = () => {
+
+  const createItems = () => {
+    const array = [];
+    for (let i = 0; i < 36; i++) {
+      array.push({hasItem: false, clicked: false});
+    }
+    const random = Math.floor(Math.random() * array.length);
+    array[random].hasItem = true;
+    return array;
+  };
+
+  const [items, setItems] = useState(createItems());
+  const [tries, setTries] = useState(0);
+  const [found, setFound] = useState(false);
+
+
+  const clickItem = (index: number) => {
+    const copyItems = [...items];
+    const copyItem = {...items[index]};
+
+    if (found) {
+      return;
+    }
+
+    if (!copyItems[index].clicked) {
+      copyItem.clicked = true;
+      copyItems[index] = copyItem;
+      setItems(copyItems);
+      setTries(tries + 1);
+    }
+
+    if (copyItems[index].hasItem) {
+      setFound(true);
+    }
+  };
+
+  const resetGame = () => {
+    setItems(createItems());
+    setTries(0);
+    setFound(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hidden Item Game</h1>
+      <PlayingField
+        items={items}
+        clickItem={clickItem}
+        found={found}
+      />
+      <TriesCounter
+        tries={tries}
+      />
+    <ResetButton
+      resetGame={resetGame}
+      />
     </div>
-  );
-}
+    );
+};
 
 export default App;
